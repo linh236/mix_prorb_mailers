36dc58e2ab45c412e7adb40a9850aaef256f57a5
Cezar Halmagean <cezar@mixandgo.com>
2022-10-26 11:37:07 +0300

    Added mailer preview w/ Maizzle

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def hello
+    user = Struct.new(:name, :email).new('John', 'jdoe@email.com')
+    UserMailer.with(user: user).hello
+  end
 end
