9bfd1172bae2387ef7a906281c3de32de466f45a
Cezar Halmagean <cezar@mixandgo.com>
2022-10-26 11:29:25 +0300

    Added email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def hello
     @user = params[:user]
-    mail(to: @user.email, subject: "Hello #{@user.name}")
+    mail(to: email_address_with_name(@user.email, @user.name), subject: "Hello #{@user.name}")
   end
 end
