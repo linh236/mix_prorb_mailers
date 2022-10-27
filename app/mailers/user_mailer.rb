3216ab95b5ff8544d06ffe90af4612b666352d16
Cezar Halmagean <cezar@mixandgo.com>
2022-10-27 10:40:03 +0300

    Adding attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def hello
     @user = params[:user]
+    attachments["flower.png"] = File.read("tmp/flower.png")
     mail(to: email_address_with_name(@user.email, @user.name), subject: "Hello #{@user.name}")
   end
 end
